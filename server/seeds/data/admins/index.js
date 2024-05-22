1485821a31372770cdd3ae8decc411c919fc9ac1
Wolf of Wall Street <a1111@Finance.local>
2024-05-23 02:02:50 +0300

    first commit

---
+++ server/seeds/data/admins/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable max-len */
+const md5 = require('md5');
+
+module.exports = {
+    login: 'crowdy',
+    password: md5('crow0098_'),
+    email: 'investmentscrowdy@gmail.com',
+    id: 'admin_id'
+};
